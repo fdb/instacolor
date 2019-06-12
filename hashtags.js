97cbdd49e82e0cb1865e3f801d4b44e36c9c9112
Frederik De Bleser <frederik@debleser.be>
2019-06-12 12:38:55 +0200

    New faster code for hashtags.

--- hashtags.js
+++ hashtags.js
@@ -1,43 +1,82 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const fetch = require("node-fetch");
 
 const PAGE_WIDTH = 1024;
 const PAGE_HEIGHT = 10000;
 
 async function getHashtagsForPost(insta, url) {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   await page.goto(url);
-  const links = await page.$$('a');
+  const links = await page.$$("a");
   const hashtags = new Set();
   for (const link of links) {
-      const hrefHandle = await link.getProperty('href');
-      const href = await hrefHandle.jsonValue();
-      if (href.includes('https://www.instagram.com/explore/tags/')) {
-          const hashtagText = await (await link.getProperty('text')).jsonValue();
-          hashtags.add(hashtagText);
-      }
+    const hrefHandle = await link.getProperty("href");
+    const href = await hrefHandle.jsonValue();
+    if (href.includes("https://www.instagram.com/explore/tags/")) {
+      const hashtagText = await (await link.getProperty("text")).jsonValue();
+      hashtags.add(hashtagText);
+    }
   }
   return Array.from(hashtags);
 }
 
 async function getHashtags(insta) {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  page.setViewport({ width: PAGE_WIDTH, height: PAGE_HEIGHT});
-  await page.goto(`https://www.instagram.com/${insta}/`);
+  const url = `https://www.instagram.com/${insta}/`;
+  const res = await fetch(url);
+  const html = await res.text();
+  //console.log(html);
+  const startIndex = html.indexOf("window._sharedData =");
+  const endIndex = html.indexOf(";</script>", startIndex);
+  // console.log(startIndex, endIndex);
+  const json = html.substring(
+    startIndex + "window._sharedData =".length,
+    endIndex
+  );
 
-  const posts = new Set();
+  // console.log(json);
 
-  const links = await page.$$('a');
-  for (const link of links) {
-      const hrefHandle = await link.getProperty('href');
-      const href = await hrefHandle.jsonValue();
-      // console.log(href);
-      if (href.includes('https://www.instagram.com/p/')) {
-          posts.add(href);
+  const hashtagMap = {};
+
+  const sharedData = JSON.parse(json);
+  const profile = sharedData.entry_data.ProfilePage[0].graphql.user;
+  console.log(profile);
+  const posts = profile.edge_owner_to_timeline_media.edges;
+  for (const post of posts) {
+    const caption = post.node.edge_media_to_caption.edges[0].node.text;
+    const re = /#(\w+)/g;
+    let m;
+    do {
+      m = re.exec(caption);
+      if (m) {
+        const hashtag = m[1];
+        let count = hashtagMap[hashtag] || 0;
+        count += 1;
+        hashtagMap[hashtag] = count;
       }
+    } while (m);
   }
-  console.log(posts);
+
+  console.log(hashtagMap);
+  // Get posts
+
+  //const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  //const page = await browser.newPage();
+  // page.setViewport({ width: PAGE_WIDTH, height: PAGE_HEIGHT });
+  // await page.goto(`https://www.instagram.com/${insta}/`);
+
+  // const posts = new Set();
+
+  // const links = await page.$$("a");
+  // for (const link of links) {
+  //   const hrefHandle = await link.getProperty("href");
+  //   const href = await hrefHandle.jsonValue();
+  //   // console.log(href);
+  //   if (href.includes("https://www.instagram.com/p/")) {
+  //     posts.add(href);
+  //   }
+  // }
+  // console.log(posts);
 
   // for (let i = 0; i < 2; i++) {
   //     const links = await page.$$('a');
@@ -56,38 +95,38 @@ async function getHashtags(insta) {
   //     await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
   //     await page.waitFor(500);
 
-      // console.log(posts);
-      // await downloadPost(insta, posts[0]);
+  // console.log(posts);
+  // await downloadPost(insta, posts[0]);
   // }
   // console.log('FINAL');
   // console.log(posts);
 
-  const hashtagMap = {};
-  const requests = [];
-  for (const postUrl of posts) {
-      requests.push(getHashtagsForPost(insta, postUrl));
-  }
-  const allHashtags = await Promise.all(requests);
-  for (const hashtags of allHashtags) {
-      for (const hashtag of hashtags) {
-          let count = hashtagMap[hashtag] || 0;
-          count += 1;
-          hashtagMap[hashtag] = count;
-      }
-  }
+  // const hashtagMap = {};
+  // const requests = [];
+  // for (const postUrl of posts) {
+  //   requests.push(getHashtagsForPost(insta, postUrl));
+  // }
+  // const allHashtags = await Promise.all(requests);
+  // for (const hashtags of allHashtags) {
+  //   for (const hashtag of hashtags) {
+  //     let count = hashtagMap[hashtag] || 0;
+  //     count += 1;
+  //     hashtagMap[hashtag] = count;
+  //   }
+  // }
 
-  console.log(hashtagMap);
-  const hashtagList = []
+  // console.log(hashtagMap);
+  const hashtagList = [];
   for (hashtag of Object.keys(hashtagMap)) {
-      const count = hashtagMap[hashtag];
-      hashtagList.push({ hashtag, count });
+    const count = hashtagMap[hashtag];
+    hashtagList.push({ hashtag, count });
   }
 
   hashtagList.sort((a, b) => b.count - a.count);
   return hashtagList;
 }
 
-module.exports = { getHashtags }
+module.exports = { getHashtags };
 
 // downloadPosts('nasa');
 // downloadPost('nasa', 'https://www.instagram.com/p/BvZZKL9jRZf/');
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-const puppeteer = require('puppeteer');
-const rgbaPalette = require('get-rgba-palette');
-const PNG = require('png-js');
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const rgbaPalette = require("get-rgba-palette");
+const PNG = require("png-js");
 
-const getHashtags = require('./hashtags').getHashtags;
+const getHashtags = require("./hashtags").getHashtags;
 
 const PAGE_HEADER_HEIGHT = 650;
 const PAGE_WIDTH = 1024;
@@ -15,42 +15,51 @@ const app = express();
 app.use(cors());
 
 function componentToHex(c) {
-    const hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+  const hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-
 async function downloadPageAsImage(insta, fileName) {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
-  page.setViewport({ width: PAGE_WIDTH, height: PAGE_HEIGHT});
+  page.setViewport({ width: PAGE_WIDTH, height: PAGE_HEIGHT });
   await page.goto(`https://www.instagram.com/${insta}/`);
   const closeButton = await page.$('[aria-label="Close"]');
   if (closeButton) {
     closeButton.click();
   }
-  await page.screenshot({path: fileName, clip: { x: 0, y: PAGE_HEADER_HEIGHT, width: PAGE_WIDTH, height: PAGE_HEIGHT - PAGE_HEADER_HEIGHT }});
+  await page.screenshot({
+    path: fileName,
+    clip: {
+      x: 0,
+      y: PAGE_HEADER_HEIGHT,
+      width: PAGE_WIDTH,
+      height: PAGE_HEIGHT - PAGE_HEADER_HEIGHT
+    }
+  });
   await browser.close();
 }
 
 async function rawExtractPalette(imageFileName) {
   return new Promise(function(resolve) {
-      PNG.decode(imageFileName, function (pixels) {
-        const colors = rgbaPalette.bins(pixels, 10);
-        resolve(colors);
-      });
+    PNG.decode(imageFileName, function(pixels) {
+      const colors = rgbaPalette.bins(pixels, 10);
+      resolve(colors);
+    });
   });
 }
 
 async function extractPalette(imageFileName, paletteFileName) {
   if (fs.existsSync(paletteFileName)) {
     return new Promise((resolve, reject) => {
-      fs.readFile(paletteFileName, 'utf-8', (err, data) => {
-        if (err) { return reject(err); }
+      fs.readFile(paletteFileName, "utf-8", (err, data) => {
+        if (err) {
+          return reject(err);
+        }
         const palette = JSON.parse(data);
         resolve(palette);
       });
@@ -75,7 +84,8 @@ async function extractPalette(imageFileName, paletteFileName) {
 
 async function handleGetPalette(insta) {
   const d = new Date();
-  const prefixFileName = `/tmp/${insta}-${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+  const prefixFileName = `/tmp/${insta}-${d.getFullYear()}-${d.getMonth() +
+    1}-${d.getDate()}`;
   const imageFileName = `${prefixFileName}.png`;
   const paletteFileName = `${prefixFileName}.json`;
   if (!fs.existsSync(imageFileName)) {
@@ -96,7 +106,7 @@ async function handleGetHashtags(insta) {
   return hashtags;
 }
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const html = `<!doctype html>
   <html>
   <head><meta charset="utf-8"><title>instakleur</title>
@@ -125,10 +135,16 @@ app.get('/', (req, res) => {
     <li><a href="/palette/therock">therock</a></li>
   </ul>
   <h2>API</h2>
+  <h3>Palettes</h3>
   <pre>/api/palette/<span style="color: #3897f0">INSTAGRAM_NAME</span></pre>
   <p>Get a color palette from an Instagram profile.</p>
   <p>Palettes are cached -- a new one is generated every day.</p>
   <p>Example: <code><a href="/api/palette/natgeo">/api/palette/natgeo</a></code></p>
+  <h3>Hashtags</h3>
+  <pre>/api/hashtags/<span style="color: #3897f0">INSTAGRAM_NAME</span></pre>
+  <p>Get the hashtags from an Instagram profile.</p>
+  <p>Hashtags are cached -- a new one is generated every day.</p>
+  <p>Example: <code><a href="/api/hashtags/natgeo">/api/hashtags/natgeo</a></code></p>
   <h2>Source Code</h2>
   <p>Source code is available on <a href="https://github.com/fdb/instakleur">GitHub</a>.</p>
   <script>
@@ -144,68 +160,79 @@ app.get('/', (req, res) => {
   res.send(html);
 });
 
-app.get('/palette/:insta', (req, res) => {
+app.get("/palette/:insta", (req, res) => {
   const insta = req.params.insta;
   handleGetPalette(insta)
     .then(palette => {
-      let html = '<!doctype html>\n';
+      let html = "<!doctype html>\n";
       html += '<html><head><meta charset="utf-8"><title>instakleur</title>\n';
-      html += '<style>\n';
-      html += 'body, html { font: 14px sans-serif; background: #fafafa; color: #666; max-width: 1000px; padding: 0 5vw; margin: 30px auto; }\n';
-      html += 'a { color: inherit; text-decoration: none; border-bottom: 2px solid rgba(0, 0, 0, 0.2); }\n'
-      html += '.wrap { display: flex; flex-direction: row; align-items: flex-start;  justify-content: flex-start; }\n';
-      html += 'svg { margin-right: 50px; }\n';
-      html += 'table { border-collapse: collapse; }\n';
-      html += 'th { text-align: left; }\n';
-      html += 'td, th { padding: 10px 30px; }\n';
-      html += 'tr { border-bottom: 1px solid #ddd; }\n';
-      html += '.swatch { display: inline-block; width: 1rem; height: 1rem; margin-right: 1rem; vertical-align: top; }\n';
-      html += '</style>\n';
-      html += '</head><body>\n';
+      html += "<style>\n";
+      html +=
+        "body, html { font: 14px sans-serif; background: #fafafa; color: #666; max-width: 1000px; padding: 0 5vw; margin: 30px auto; }\n";
+      html +=
+        "a { color: inherit; text-decoration: none; border-bottom: 2px solid rgba(0, 0, 0, 0.2); }\n";
+      html +=
+        ".wrap { display: flex; flex-direction: row; align-items: flex-start;  justify-content: flex-start; }\n";
+      html += "svg { margin-right: 50px; }\n";
+      html += "table { border-collapse: collapse; }\n";
+      html += "th { text-align: left; }\n";
+      html += "td, th { padding: 10px 30px; }\n";
+      html += "tr { border-bottom: 1px solid #ddd; }\n";
+      html +=
+        ".swatch { display: inline-block; width: 1rem; height: 1rem; margin-right: 1rem; vertical-align: top; }\n";
+      html += "</style>\n";
+      html += "</head><body>\n";
       html += `<h1><a href="/">instakleur</a> – <a href="https://instagram.com/${insta}/">${insta}</a></h1>\n`;
-      let svg = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="500" viewBox="0 0 200 500">\n';
-      let table = '<table><tr><th>Color</th><th>Ratio</th></tr>';
+      let svg =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="500" viewBox="0 0 200 500">\n';
+      let table = "<table><tr><th>Color</th><th>Ratio</th></tr>";
       let y = 0;
       for (const swatch of palette) {
         const swatchHeight = swatch.ratio * 500;
-        svg += `  <rect x="0" y="${y}" width="200" height="${swatchHeight}" fill="${swatch.color}" />\n`;
-        table += `  <tr><td><span class="swatch" style="background-color: ${swatch.color}"></span>${swatch.color}</td><td>${(swatch.ratio * 100).toFixed(2)}%</td></tr>\n`;
+        svg += `  <rect x="0" y="${y}" width="200" height="${swatchHeight}" fill="${
+          swatch.color
+        }" />\n`;
+        table += `  <tr><td><span class="swatch" style="background-color: ${
+          swatch.color
+        }"></span>${swatch.color}</td><td>${(swatch.ratio * 100).toFixed(
+          2
+        )}%</td></tr>\n`;
         y += swatchHeight;
       }
-      svg += '</svg>\n';
-      table += '</table>\n';
+      svg += "</svg>\n";
+      table += "</table>\n";
       html += '<div class="wrap">\n';
       html += svg;
       html += table;
-      html += '</div>\n';
-      html += '</body></html>\n';
+      html += "</div>\n";
+      html += "</body></html>\n";
       res.send(html);
     })
     .catch(error => {
-      res.send({ status: 'error', error: error.toString() });
-    })
+      res.send({ status: "error", error: error.toString() });
+    });
 });
 
-app.get('/api/palette/:insta', (req, res) => {
+app.get("/api/palette/:insta", (req, res) => {
   const insta = req.params.insta;
   handleGetPalette(insta)
     .then(palette => {
-      res.send({ status: 'ok', palette });
+      res.send({ status: "ok", palette });
     })
     .catch(error => {
-      res.send({ status: 'error', error: error.toString() });
-    })
-})
+      res.send({ status: "error", error: error.toString() });
+    });
+});
 
-app.get('/api/hashtags/:insta', (req, res) => {
+app.get("/api/hashtags/:insta", (req, res) => {
   const insta = req.params.insta;
   handleGetHashtags(insta)
     .then(hashtags => {
-      res.send({ status: 'ok', hashtags });
+      res.send({ status: "ok", hashtags });
     })
     .catch(error => {
-      res.send({ status: 'error', error: error.toString() });
-    })
+      res.send({ status: "error", error: error.toString() });
+    });
 });
 
 const PORT = process.env.PORT || 4000;
